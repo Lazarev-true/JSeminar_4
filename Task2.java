46c0d94313cf7e41a0bed87e1948db5681f35eb6
Konstantin <klik@mail.ru>
2022-12-30 16:40:34 +0300

    JSeminar_4

---
+++ Task2.java
@@ -0,0 +1,27 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+public class Task2 {
+
+    // Дан Deque состоящий из последовательности цифр.
+    // Необходимо проверить, что последовательность цифр является палиндромом
+    public static void main(String[] args) throws Exception {
+        Deque<Integer> deque = new ArrayDeque<>(Arrays.asList(1,2,3,3,2,4));
+        Task2 first = new Task2();
+        
+            System.out.println(first.checkOn(deque));
+    }
+        
+
+
+    public boolean checkOn(Deque<Integer> deque) throws Exception {
+        boolean result = true;
+        if (deque == null) throw new Exception("Коллекция ссылается на null");
+        while (result && deque.size()>1)
+        {
+            result = result && (deque.pollFirst().equals(deque.pollLast()));
+        }
+        return result;
+    }
+}
